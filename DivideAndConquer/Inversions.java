8712846ab6ad4c9cf4d8e78acd82ea144eba6e80
Omar Ham <omar.hp90@gmail.com>
2016-11-28 22:54:49 -0600

    Add invertions solution

--- DivideAndConquer/Inversions.java
+++ DivideAndConquer/Inversions.java
@@ -11,7 +11,44 @@ public class Inversions {
         numberOfInversions += getNumberOfInversions(a, b, left, ave);
         numberOfInversions += getNumberOfInversions(a, b, ave, right);
         //write your code here
+        numberOfInversions+=merge(a, b, left, ave, right);
         return numberOfInversions;
+    }
+
+    public static long merge(int a[], int b[], int left, int ave, int right){
+        int i=left, k=left;
+        int j=ave;
+        long counter=0;
+        while(i<ave && j<right){
+            if(a[i]<=a[j]){
+                b[k]=a[i];
+                k++;
+                i++;
+            }else{
+                b[k]=a[j];
+                j++;
+                k++;
+                counter+=ave-i;
+            }
+        }
+
+        while(i<ave){
+            b[k]=a[i];
+            i++;
+            k++;
+        }
+
+        while(j<right){
+            b[k]=a[j];
+            j++;
+            k++;
+        }
+
+        for(int q=left; q<right; q++){
+            a[q]=b[q];
+        }
+
+        return counter;
     }
 
     public static void main(String[] args) {
